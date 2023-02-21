109ff87f2877763d54496a20ba396524670b92e5
guitisch <guitisch@gmail.com>
2023-02-21 14:27:37 +0100

    Changement titre exo1

--- exe1.php
+++ exe1.php
@@ -1,4 +1,4 @@
-<h1>exe1</h1>
+<h1>Exercice 1</h1>
 Soit la phrase « Notre formation DL commence aujourd'hui ».
 Ecrire un algorithme permettant de compter le nombre de caractères contenus dans cette phrase
 (espaces inclus).
